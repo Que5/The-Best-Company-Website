a90f4c57975db898e4234ae5ffb38bd18b011ddb
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-21 13:52:34 +0200

    Added a selectbox

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,10 +1,21 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Us")
 
 with st.form(key="my_form"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
+    options = st.selectbox("What topic do you want to discuss?",
+                           ('Job Enquires', 'Project Proposals', 'Other'))
+    raw_message = st.text_area("Text")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+Topic {options}
+{raw_message}"""
+    
     button = st.form_submit_button("Submit")
     if button:
-        message = message + user_email
+        send_email(message)
+        st.info("Your email was sent successfully!")
--- send_email.py
+++ send_email.py
@@ -1,20 +1,21 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_mail(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "phumlanibrightdlamini@gmail.com"
-password = "agub ucpe rrpz jhfx"
+    username = "phumlanibrightdlamini@gmail.com"
+    password = "agub ucpe rrpz jhfx"
 
-receiver = "phumlanibrightdlamini@gmail.com"
+    receiver = "phumlanibrightdlamini@gmail.com"
 
-context = ssl.create_default_context()
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!"""
+    message = """\
+    Subject: Hi!
+    How are you?
+    Bye!"""
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username,receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username,receiver, message)
